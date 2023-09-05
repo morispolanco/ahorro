770862776eb5036408b2d25f12e6f5d3c002845e
morispolanco <morispolanco@gmail.com>
2023-09-04 20:36:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,18 @@ with st.sidebar:
 
 st.title("💬 Chatbot") 
 
-# Define el prompt sin mostrarlo al usuario
-ahorro_prompt = "Proporcione cinco consejos diarios específicos y prácticos para ahorrar dinero en alimentos, transporte, ocio, servicios públicos y compras. Incluya explicaciones y ahorros estimados para cada consejo, discuta su efectividad y beneficios financieros a largo plazo. Asegúrese de que los consejos sean realistas, ampliamente aplicables y promuevan ahorros sostenibles."
+# Define los prompts para proporcionar consejos de ahorro en cinco áreas diferentes
+prompts = [
+    "Proporcione un consejo diario específico y práctico para ahorrar dinero en alimentos. Incluya una explicación y el ahorro estimado para este consejo.",
+    "Proporcione un consejo diario específico y práctico para ahorrar dinero en transporte. Incluya una explicación y el ahorro estimado para este consejo.",
+    "Proporcione un consejo diario específico y práctico para ahorrar dinero en ocio. Incluya una explicación y el ahorro estimado para este consejo.",
+    "Proporcione un consejo diario específico y práctico para ahorrar dinero en servicios públicos. Incluya una explicación y el ahorro estimado para este consejo.",
+    "Proporcione un consejo diario específico y práctico para ahorrar dinero en compras. Incluya una explicación y el ahorro estimado para este consejo."
+]
 
 if "messages" not in st.session_state:
     # El asistente comienza la conversación con un mensaje de bienvenida
-    st.session_state["messages"] = [{"role": "assistant", "content": "¡Hola! Estoy aquí para ayudarte con consejos de ahorro en tu vida diaria. ¿En qué área específica te gustaría recibir consejos hoy?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "¡Hola! Estoy aquí para proporcionarte consejos diarios sobre cómo ahorrar dinero en diferentes áreas. ¿En cuál de las siguientes áreas te gustaría recibir un consejo: alimentos, transporte, ocio, servicios públicos o compras?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -26,10 +32,19 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    # Agregar el prompt de ahorro en cada interacción para que el modelo lo use internamente
-    st.session_state.messages.append({"role": "assistant", "content": ahorro_prompt})
-    
-    # La conversación continuará utilizando el prompt de ahorro sin mostrarlo al usuario.
+    # Agregar los prompts de consejos en función de la elección del usuario
+    if "alimentos" in prompt.lower():
+        st.session_state.messages.append({"role": "assistant", "content": prompts[0]})
+    elif "transporte" in prompt.lower():
+        st.session_state.messages.append({"role": "assistant", "content": prompts[1]})
+    elif "ocio" in prompt.lower():
+        st.session_state.messages.append({"role": "assistant", "content": prompts[2]})
+    elif "servicios públicos" in prompt.lower():
+        st.session_state.messages.append({"role": "assistant", "content": prompts[3]})
+    elif "compras" in prompt.lower():
+        st.session_state.messages.append({"role": "assistant", "content": prompts[4]})
+
+    # La conversación continuará proporcionando consejos en la categoría elegida.
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
