004cb6e2b7497c934684de7e4f8de2d1af900c7f
morispolanco <morispolanco@gmail.com>
2023-09-04 20:49:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import openai 
 import streamlit as st
-import pandas as pd
-
-# Define la función para calcular el ahorro total
-def calcular_ahorro_total(consejos):
-    ahorro_total = sum(consejo.get("ahorro_estimado", 0) for consejo in consejos)
-    return ahorro_total
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -21,6 +15,35 @@ prompts = [
     "Proporcione un consejo diario específico y práctico para ahorrar dinero en servicios públicos. Incluya una explicación y el ahorro estimado para este consejo.",
     "Proporcione un consejo diario específico y práctico para ahorrar dinero en compras. Incluya una explicación y el ahorro estimado para este consejo."
 ]
+
+# Define los elementos y ahorros estimados en cada categoría
+elementos_y_ahorros = {
+    "alimentos": [
+        {"elemento": "Comer en casa en lugar de restaurantes", "ahorro_estimado": 50},
+        {"elemento": "Comprar alimentos a granel", "ahorro_estimado": 20},
+        {"elemento": "Planificar comidas con anticipación", "ahorro_estimado": 30}
+    ],
+    "transporte": [
+        {"elemento": "Usar transporte público en lugar de auto", "ahorro_estimado": 40},
+        {"elemento": "Caminar o andar en bicicleta cuando sea posible", "ahorro_estimado": 20},
+        {"elemento": "Compartir viajes con amigos o colegas", "ahorro_estimado": 25}
+    ],
+    "ocio": [
+        {"elemento": "Optar por actividades gratuitas o de bajo costo", "ahorro_estimado": 30},
+        {"elemento": "Reducir la suscripción a servicios de transmisión", "ahorro_estimado": 15},
+        {"elemento": "Buscar ofertas y descuentos en entretenimiento", "ahorro_estimado": 20}
+    ],
+    "servicios públicos": [
+        {"elemento": "Reducir el consumo de electricidad apagando luces y dispositivos", "ahorro_estimado": 40},
+        {"elemento": "Aislar ventanas y puertas para mejorar la eficiencia energética", "ahorro_estimado": 25},
+        {"elemento": "Programar el termostato para un uso eficiente de la calefacción y refrigeración", "ahorro_estimado": 35}
+    ],
+    "compras": [
+        {"elemento": "Comparar precios antes de realizar compras grandes", "ahorro_estimado": 50},
+        {"elemento": "Utilizar cupones y descuentos en línea", "ahorro_estimado": 20},
+        {"elemento": "Comprar productos genéricos en lugar de marcas de lujo", "ahorro_estimado": 30}
+    ]
+}
 
 # Lista para almacenar los consejos y ahorros estimados
 consejos_y_ahorros = []
@@ -44,14 +67,19 @@ if prompt := st.chat_input():
     # Agregar los prompts de consejos en función de la elección del usuario
     if "alimentos" in prompt.lower():
         st.session_state.messages.append({"role": "assistant", "content": prompts[0]})
+        consejos = elementos_y_ahorros["alimentos"]
     elif "transporte" in prompt.lower():
         st.session_state.messages.append({"role": "assistant", "content": prompts[1]})
+        consejos = elementos_y_ahorros["transporte"]
     elif "ocio" in prompt.lower():
         st.session_state.messages.append({"role": "assistant", "content": prompts[2]})
+        consejos = elementos_y_ahorros["ocio"]
     elif "servicios públicos" in prompt.lower():
         st.session_state.messages.append({"role": "assistant", "content": prompts[3]})
+        consejos = elementos_y_ahorros["servicios públicos"]
     elif "compras" in prompt.lower():
         st.session_state.messages.append({"role": "assistant", "content": prompts[4]})
+        consejos = elementos_y_ahorros["compras"]
 
     # La conversación continuará proporcionando consejos en la categoría elegida.
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
@@ -59,22 +87,12 @@ if prompt := st.chat_input():
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
 
-    # Extraer y almacenar los consejos y ahorros estimados
-    consejo_actual = msg.content
-    ahorro_estimado = 0  # Deberás analizar el consejo para extraer el ahorro estimado, por ejemplo, usando expresiones regulares.
-    consejos_y_ahorros.append({"consejo": consejo_actual, "ahorro_estimado": ahorro_estimado})
+    # Mostrar los consejos y ahorros estimados con la opción de revelar el ahorro al hacer clic en un botón
+    for consejo in consejos:
+        if st.button(f"Revelar ahorro para '{consejo['elemento']}'"):
+            st.info(f"Ahorro estimado para '{consejo['elemento']}': ${consejo['ahorro_estimado']}")
 
-# Preguntar al usuario si desea ver una tabla con el ahorro total
-if st.button("Ver tabla de ahorro total"):
-    # Calcular el ahorro total
-    ahorro_total = calcular_ahorro_total(consejos_y_ahorros)
-    
-    # Crear un DataFrame con los consejos y ahorros estimados
-    df = pd.DataFrame(consejos_y_ahorros)
-    
-    # Mostrar la tabla con los consejos y ahorros estimados
-    st.subheader("Tabla de ahorro total")
-    st.dataframe(df)
-    
-    # Mostrar el ahorro total calculado
-    st.info(f"Ahorro total estimado: ${ahorro_total}")
+# Mostrar el ahorro total calculado
+if st.button("Calcular ahorro total"):
+    ahorro_total = sum(consejo.get("ahorro_estimado", 0) for consejo in consejos)
+    st.info(f"Ahorro total estim
