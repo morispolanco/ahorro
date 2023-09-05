805a519491bdd255a8b95dde6fdd737e0bfb572b
morispolanco <morispolanco@gmail.com>
2023-09-04 20:43:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import openai 
 import streamlit as st
+import pandas as pd
+
+# Define la función para calcular el ahorro total
+def calcular_ahorro_total(consejos):
+    ahorro_total = sum(consejo.get("ahorro_estimado", 0) for consejo in consejos)
+    return ahorro_total
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -15,6 +21,9 @@ prompts = [
     "Proporcione un consejo diario específico y práctico para ahorrar dinero en servicios públicos. Incluya una explicación y el ahorro estimado para este consejo.",
     "Proporcione un consejo diario específico y práctico para ahorrar dinero en compras. Incluya una explicación y el ahorro estimado para este consejo."
 ]
+
+# Lista para almacenar los consejos y ahorros estimados
+consejos_y_ahorros = []
 
 if "messages" not in st.session_state:
     # El asistente comienza la conversación con un mensaje de bienvenida
@@ -49,3 +58,23 @@ if prompt := st.chat_input():
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
+
+    # Extraer y almacenar los consejos y ahorros estimados
+    consejo_actual = msg.content
+    ahorro_estimado = 0  # Deberás analizar el consejo para extraer el ahorro estimado, por ejemplo, usando expresiones regulares.
+    consejos_y_ahorros.append({"consejo": consejo_actual, "ahorro_estimado": ahorro_estimado})
+
+# Preguntar al usuario si desea ver una tabla con el ahorro total
+if st.button("Ver tabla de ahorro total"):
+    # Calcular el ahorro total
+    ahorro_total = calcular_ahorro_total(consejos_y_ahorros)
+    
+    # Crear un DataFrame con los consejos y ahorros estimados
+    df = pd.DataFrame(consejos_y_ahorros)
+    
+    # Mostrar la tabla con los consejos y ahorros estimados
+    st.subheader("Tabla de ahorro total")
+    st.dataframe(df)
+    
+    # Mostrar el ahorro total calculado
+    st.info(f"Ahorro total estimado: ${ahorro_total}")
